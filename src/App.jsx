dbf5695fbdd1cbf97c3b9d3443bc5f0b55db5da3
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-16 18:10:20 +0200

    feat: add context favourites

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import Homepage from './pages/Homepage';
 import CoffeeDetailPage from './pages/CoffeeDetailPage';
+import { FavouriteProvider } from './contexts/FavoriteContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/coffees/:id' element={<CoffeeDetailPage />} />
-        </Route>
-      </Routes>
+      <FavouriteProvider>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/coffees/:id' element={<CoffeeDetailPage />} />
+          </Route>
+        </Routes>
+      </FavouriteProvider>
     </BrowserRouter>
   );
 }
---
+++ src/contexts/FavoriteContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from 'react';
+
+
+const FavouriteContext = createContext();
+
+function FavouriteProvider({ children }) {
+    const [favourites, setFavourites] = useState({});
+    return (
+        <FavouriteContext.Provider value={{ favourites, setFavourites }}>
+            {children}
+        </FavouriteContext.Provider>
+    );
+}
+
+function useFavourite() {
+    const context = useContext(FavouriteContext);
+    return context;
+}
+
+export { FavouriteProvider, useFavourite };
