523296b1b9ecc8afeb3ff734b642116ad1d8e7a6
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-17 14:08:45 +0200

    feat: add types.ts

---
+++ types.ts
@@ -0,0 +1,14 @@
+export type Coffee = {
+    title: string;
+    category: string;
+    brand?: string;
+    origin?: string;
+    roastLevel?: "Chiaro" | "Medio" | "Scuro";
+    price?: number;
+    weight?: number;
+    caffeineContent?: number;
+    flavorNotes?: string[];
+    intensity?: number;
+    isDecaf?: boolean;
+    imageUrl: string;
+};
