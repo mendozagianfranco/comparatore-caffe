b48cf0ee1751392d4c1391e2341f54f92178d849
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-18 12:39:12 +0200

    feat: change data property

--- types.ts
+++ types.ts
@@ -1,14 +1,15 @@
 export type Coffee = {
     title: string;
     category: string;
-    brand?: string;
-    origin?: string;
+    brand: string;
+    origin: string;
     roastLevel?: "Chiaro" | "Medio" | "Scuro";
     price?: number;
     weight?: number;
-    caffeineContent?: number;
+    caffeineContent: number;
     flavorNotes?: string[];
-    intensity?: number;
+    intensity: number;
+    description: string;
     isDecaf?: boolean;
     imageUrl: string;
 };
