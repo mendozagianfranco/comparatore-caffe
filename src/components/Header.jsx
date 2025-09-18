96e3529b027c9465ab8445ca923c0585362efe88
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-18 13:23:37 +0200

    feat: add number in favourti header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,10 @@ export default function Header() {
         <nav className="navbar sticky-top ">
             <div className="container-fluid">
                 <Link className="fs-4 title" to={'/'}>SorSo a SorSo</Link>
-                <span data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" role='button'><i className="fa-solid fa-heart text-danger me-1"></i>Preferiti</span>
-
+                <span data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" role='button'><i className="fa-solid fa-heart text-danger me-1"></i>
+                    Preferiti
+                    {favourites.length > 0 && <span className="position-absolute  start-150 translate-middle badge rounded-pill bg-danger">{favourites.length}</span>}
+                </span>
                 <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
                     <div className="offcanvas-header">
                         <h5 className="offcanvas-title" id="offcanvasRightLabel">Preferiti</h5>
