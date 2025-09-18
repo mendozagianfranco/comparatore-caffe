a6f18d2a823b5e168fc94b11a32197f850378a27
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-09-18 13:09:12 +0200

    feat: add CompareList to DefaultLayout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
+import CompareList from '../components/CompareList';
 
 
 export default function DefaultLayout() {
@@ -9,6 +10,7 @@ export default function DefaultLayout() {
             <main>
                 <Outlet />
             </main>
+            <CompareList />
         </>
     );
 }
